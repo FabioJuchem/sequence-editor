36e720cbb31d2c3c9363a6e6b0f96895092ab1a9
FabioJuchem <fabiojuchem@gmail.com>
2022-08-11 19:26:27 -0300

    deploy2

--- appmain/src/main/kotlin/com/fabiojuchem/appmain/AppmainApplication.kt
+++ appmain/src/main/kotlin/com/fabiojuchem/appmain/AppmainApplication.kt
@@ -6,7 +6,7 @@ import org.springframework.context.annotation.ComponentScan
 import org.springframework.web.reactive.config.EnableWebFlux
 
 @SpringBootApplication
-@ComponentScan(basePackages = ["com.fabiojuchem"])
+@ComponentScan
 @EnableWebFlux
 class AppmainApplication
 
