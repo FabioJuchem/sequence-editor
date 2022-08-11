b3cbe9d071c800f145457fa9505c8d907a61d95d
FabioJuchem <fabiojuchem@gmail.com>
2022-08-11 18:22:08 -0300

    Fix

--- appmain/src/main/kotlin/com/fabiojuchem/appmain/AppmainApplication.kt
+++ appmain/src/main/kotlin/com/fabiojuchem/appmain/AppmainApplication.kt
@@ -1,11 +1,13 @@
 package com.fabiojuchem.appmain
 
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.context.annotation.ComponentScan
 import org.springframework.web.reactive.config.EnableWebFlux
 
 @SpringBootApplication
+@EnableAutoConfiguration
 @ComponentScan(basePackages = ["com.fabiojuchem.appmain", "com.fabiojuchem.authapi", "com.fabiojuchem.common", "com.fabiojuchem.processorapi"])
 @EnableWebFlux
 class AppmainApplication
