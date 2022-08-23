2b7173d50c73c1c141e2f5929dd988358bd92f78
FabioJuchem <fabiojuchem@gmail.com>
2022-08-23 20:11:13 -0300

    Fix

--- appmain/src/main/kotlin/com/fabiojuchem/sequenceeditor/AppmainApplication.kt
+++ appmain/src/main/kotlin/com/fabiojuchem/sequenceeditor/AppmainApplication.kt
@@ -5,11 +5,11 @@ import org.springframework.boot.runApplication
 import org.springframework.context.annotation.ComponentScan
 import org.springframework.web.reactive.config.EnableWebFlux
 
-//@SpringBootApplication
-//@ComponentScan(basePackages = ["com.fabiojuchem"])
-//@EnableWebFlux
-//class AppmainApplication
-//
-//fun main(args: Array<String>) {
-//	runApplication<AppmainApplication>(*args)
-//}
+@SpringBootApplication
+@ComponentScan(basePackages = ["com.fabiojuchem"])
+@EnableWebFlux
+class AppmainApplication
+
+fun main(args: Array<String>) {
+	runApplication<AppmainApplication>(*args)
+}
--- appmain/src/main/kotlin/com/fabiojuchem/sequenceeditor/ScanAppmainApplication.kt
+++ appmain/src/main/kotlin/com/fabiojuchem/sequenceeditor/ScanAppmainApplication.kt
@@ -5,9 +5,9 @@ import org.springframework.context.annotation.ComponentScan
 import org.springframework.data.r2dbc.repository.config.EnableR2dbcRepositories
 import org.springframework.stereotype.Component
 
-//@Component
-//@ComponentScan(basePackageClasses = [ScanAppmainApplication::class])
-//@EntityScan(basePackageClasses = [ScanAppmainApplication::class])
-//@EnableR2dbcRepositories(basePackageClasses = [ScanAppmainApplication::class])
-//class ScanAppmainApplication {
-//}
+@Component
+@ComponentScan(basePackageClasses = [ScanAppmainApplication::class])
+@EntityScan(basePackageClasses = [ScanAppmainApplication::class])
+@EnableR2dbcRepositories(basePackageClasses = [ScanAppmainApplication::class])
+class ScanAppmainApplication {
+}
